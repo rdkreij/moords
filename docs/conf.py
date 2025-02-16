b6a313583afff9ec71cbdddcee4b4b0aec68b07c
Rick de Kreij <r.d.kreij@gmail.com>
2025-02-17 00:43:27 +0100

    auto docs

--- docs/conf.py
+++ docs/conf.py
@@ -9,29 +9,28 @@ import sys
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'moords'
-copyright = '2025, Rick de Kreij'
-author = 'Rick de Kreij'
+project = "moords"
+copyright = "2025, Rick de Kreij"
+author = "Rick de Kreij"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-sys.path.insert(0, os.path.abspath('../src'))  # Add path to your package
+sys.path.insert(0, os.path.abspath("../src"))  # Add path to your package
 
 # Add extensions for Sphinx to use
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',  # To support Google and NumPy style docstrings
-    'sphinx_autodoc_typehints',  # For type hinting
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",  # To support Google and NumPy style docstrings
+    "sphinx_autodoc_typehints",  # For type hinting
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
